ba0b3ed259710152e687f203c4170eeffc584b99
Jonathan Strickland <djanatyn@gmail.com>
2023-10-30 10:09:20 -0400

    next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  transpilePackages: ["@slippilab/parser"],
+  experimental: {
+    urlImports: ["https://cdn.skypack.dev/"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
