6f46e786333a11fb5a48c35370c200983724804e
Jonathan Strickland <djanatyn@gmail.com>
2023-11-21 11:57:31 -0500

    break into components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,6 +41,29 @@ export type GameRecord = {
   file: String;
 };
 
+enum CounterState {
+  GetSlippiTag,
+  LoadSLPFiles,
+  AnalyzeReplays,
+  DisplayResults,
+}
+
+const GetSlippiTag: React.FC<{}> = () => {
+  return (
+    <div>
+      <p>Include turnip pulls by:{"  "}</p>
+      <input
+        type="text"
+        className="border border-b p-1"
+        defaultValue="ACAB#420"
+      />
+      <button className="bg-pink-500 p-2 rounded-md text-white font-bold">
+        Add
+      </button>
+    </div>
+  );
+};
+
 const GameDisplay: React.FC<{
   games: GameRecord[];
   removeGame: (record: GameRecord) => void;
@@ -67,108 +90,128 @@ const GameDisplay: React.FC<{
   );
 };
 
-const TurnipCounter: NextPage = () => {
+const SelectReplays: React.FC<{}> = () => {
   const directoryRef = useRef<HTMLInputElement | null>(null);
   const fileRef = useRef<HTMLInputElement | null>(null);
 
+  return (
+    <>
+      <div className="flex flex-row items-center justify-between w-full">
+        {/* @ts-ignore */}
+        <input
+          type="file"
+          name="fileList"
+          webkitdirectory=""
+          id="replayDataDirectory"
+          ref={directoryRef}
+        />
+        <button className="px-4 py-2 rounded-md border-b border-indigo-500 bg-indigo-500 text-white">
+          Add Replays from Directory
+        </button>
+      </div>
+      <div className="flex flex-row items-center justify-between w-full">
+        <input
+          type="file"
+          id="replayDataFile"
+          ref={fileRef}
+        />
+        <button
+          className="px-4 py-2 rounded-md border-b border-indigo-500 bg-indigo-500 text-white"
+          onClick={async () => {
+            {
+              /* if (directoryRef.current && directoryRef.current.files) {
+              const file = await directoryRef.current.files[0].arrayBuffer();
+              setGames([...games, {
+              game: new Game(file),
+              file: directoryRef.current.files[0].name,
+              }]);
+              directoryRef.current.value = "";
+              } */
+            }
+          }}
+        >
+          Add Replay File
+        </button>
+      </div>
+    </>
+  );
+};
+
+const Header: React.FC<{}> = () => {
+  return (
+    <div className="w-full min-h-fit h-[10vh] bg-pink-200 overflow-auto flex flex-row gap-8 justify-center items-center">
+      <p className="text-4xl font-bold">Turnip Counter</p>
+      <p className="text-xl">Analyze Melee Peach Item Pull RNG</p>
+    </div>
+  );
+};
+
+const Footer: React.FC<{}> = () => {
+  return (
+    <div className="h-[10vh] bg-pink-200 w-full flex flex-row justify-center items-center gap-2">
+      <p>
+        Built by{" "}
+        <a href="https://github.com/djanatyn" className="text-pink-900">
+          DJAN
+        </a>{" "}
+        with <a href="https://nextjs.org/" className="text-pink-900">next.js</a>
+        {" and "}
+        <a
+          href="https://www.npmjs.com/package/@slippilab/parser"
+          className="text-pink-900"
+        >
+          @slippilab/parser
+        </a>
+      </p>
+    </div>
+  );
+};
+
+const Body: React.FC<{
+  games: GameRecord[];
+  setGames: (games: GameRecord[]) => void;
+  state: CounterState;
+  setState: (state: CounterState) => void;
+}> = ({ games, setGames, state, setState }) => {
+  return (
+    <div
+      className="flex flex-col gap-4 w-full px-24 h-[80vh] bg-white"
+      style={{
+        backgroundImage: `url("/turnip-icon.png")`,
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <div className="mt-4" />
+      <p>
+        All replays are processed locally in your browser - the replays do not
+        leave your computer, and are not uploaded anywhere.
+      </p>
+      <GameDisplay
+        games={games}
+        removeGame={(remove: GameRecord) =>
+          setGames(
+            games.filter((game: GameRecord) => game.file != remove.file),
+          )}
+      />
+    </div>
+  );
+};
+
+const TurnipCounter: NextPage = () => {
   const [games, setGames] = useState<GameRecord[]>([]);
+  const [state, setState] = useState<CounterState>(CounterState.GetSlippiTag);
 
   return (
     <main className="min-h-screen flex flex-col items-center">
-      {/* header */}
-      <div className="w-full min-h-fit h-[10vh] bg-pink-200 overflow-auto flex flex-row gap-8 justify-center items-center">
-        <p className="text-4xl font-bold">Turnip Counter</p>
-        <p className="text-xl">Analyze Melee Peach Item Pull RNG</p>
-      </div>
-
-      {/* body */}
-      <div
-        className="flex flex-col gap-4 w-full px-24 h-[80vh] bg-white"
-        style={{
-          backgroundImage: `url("/turnip-icon.png")`,
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-        }}
-      >
-        <div className="mt-4" />
-        <p>
-          All replays are processed locally in your browser - the replays do not
-          leave your computer, and are not uploaded anywhere.
-        </p>
-        {/* upload slps */}
-        <div className="flex flex-row items-center justify-between w-full">
-          {/* @ts-ignore */}
-          <input
-            type="file"
-            name="fileList"
-            webkitdirectory=""
-            id="replayDataDirectory"
-            ref={directoryRef}
-          />
-          <button className="px-4 py-2 rounded-md border-b border-indigo-500 bg-indigo-500 text-white">
-            Add Replays from Directory
-          </button>
-        </div>
-        <div className="flex flex-row items-center justify-between w-full">
-          <input
-            type="file"
-            id="replayDataFile"
-            ref={fileRef}
-          />
-          <button
-            className="px-4 py-2 rounded-md border-b border-indigo-500 bg-indigo-500 text-white"
-            onClick={async () => {
-              if (directoryRef.current && directoryRef.current.files) {
-                const file = await directoryRef.current.files[0].arrayBuffer();
-                setGames([...games, {
-                  game: new Game(file),
-                  file: directoryRef.current.files[0].name,
-                }]);
-                directoryRef.current.value = "";
-              }
-            }}
-          >
-            Add Replay File
-          </button>
-        </div>
-        <div className="flex flex-row gap-2 items-center">
-          Include turnip pulls by:{"  "}
-          <input
-            type="text"
-            className="border border-b p-1"
-            defaultValue="ACAB#420"
-          />
-          <button className="bg-pink-500 p-2 rounded-md text-white font-bold">
-            Add
-          </button>
-        </div>
-        <GameDisplay
-          games={games}
-          removeGame={(remove: GameRecord) =>
-            setGames(
-              games.filter((game: GameRecord) => game.file != remove.file),
-            )}
-        />
-      </div>
-
-      {/* footer */}
-      <div className="h-[10vh] bg-pink-200 w-full flex flex-row justify-center items-center gap-2">
-        <p>
-          Built by{" "}
-          <a href="https://github.com/djanatyn" className="text-pink-900">
-            DJAN
-          </a>{" "}
-          with{" "}
-          <a href="https://nextjs.org/" className="text-pink-900">next.js</a>
-          {" and "}
-          <a
-            href="https://www.npmjs.com/package/@slippilab/parser"
-            className="text-pink-900"
-          >
-            @slippilab/parser
-          </a>
-        </p>
-      </div>
+      <Header />
+      <Body
+        games={games}
+        setGames={setGames}
+        state={state}
+        setState={setState}
+      />
+      <Footer />
     </main>
   );
 };
